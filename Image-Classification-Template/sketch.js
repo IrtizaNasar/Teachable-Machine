f8c13e41229e2ff984a428a8fe8d9761b7755b28
Irtiza Nasar <irtizanasar@gmail.com>
2024-02-12 01:53:04 +0000

    Clean

--- Image-Classification-Template/sketch.js
+++ Image-Classification-Template/sketch.js
@@ -1,7 +1,7 @@
 // Teachable Machine
-// The Coding Train / Daniel Shiffman
+// Adapted from The Coding Train / Daniel Shiffman
 // https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
-// https://editor.p5js.org/codingtrain/sketches/PoZXqbu4v
+
 
 // The video
 let video;
@@ -9,9 +9,9 @@ let video;
 let label = "waiting...";
 // The classifier
 let classifier;
-let modelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+let modelURL = ''; // Replace with model URL
 
-// STEP 1: Load the model!
+// STEP 1: Load the model! Before loading anything else 
 function preload() {
   classifier = ml5.imageClassifier(modelURL + 'model.json');
 }
@@ -34,28 +34,24 @@ function classifyVideo() {
 function draw() {
   background(0);
 
-  // Draw the video
+  // Draw the video on canvas
   image(video, 0, 0);
 
-  // STEP 4: Draw the label
+  // STEP 4: Draw the label on canvas
   textSize(32);
   textAlign(CENTER, CENTER);
   fill(255);
   text(label, width / 2, height - 16);
+  fill(0);
+  
 
-  // Pick an emoji, the "default" is train
-  let emoji = "🚂";
-  if (label == "Rainbow") {
-    emoji = "🌈";
-  } else if (label == "Unicorn") {
-    emoji = "🦄";
-  } else if (label == "Ukulele") {
-    emoji = "🎸";
-  }
 
-  // Draw the emoji
-  textSize(256);
-  text(emoji, width / 2, height / 2);
+  // STEP 5: Insert your model labels and instructions 
+  if (label == "") {
+      //Instruction to do something
+  } else if (label == "") {
+    //Instruction to do something
+  } 
 }
 
 // STEP 3: Get the classification!
@@ -66,6 +62,7 @@ function gotResults(error, results) {
     return;
   }
   // Store the label and classify again!
-  label = results[0].label;
+  label = results[0].label; // By accessing results[0].label, we are retrieving the label with the highest confidence score from the array and assigning it to the label variable. 
+  console.log(results[0]);
   classifyVideo();
 }
